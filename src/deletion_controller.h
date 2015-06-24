88d76b56fecf6c471a3832e6508d9b270522bea7
Wouter vd Heuvel <w.j.van.den.heuvel@xmsnet.nl>
2015-06-24 15:13:43 +0200

    oops

---
+++ src/deletion_controller.h
@@ -0,0 +1,17 @@
+#ifndef __DELETION_CONTROLLER_H
+#define __DELETION_CONTROLLER_H 
+
+#include "pRTOS.h"
+
+class DeletionController : public RTOS::task
+{
+public:
+	DeletionController(void)
+	{}
+
+private:
+	
+};
+
+
+#endif
